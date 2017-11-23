9e1c08869110f2bff459b469fea350945b82d959
EriccLiu <1486587187@qq.com>
2017-11-23 17:09:55 +0800

    use file

--- BuildInvFile/BuildInvFile.cpp
+++ BuildInvFile/BuildInvFile.cpp
@@ -46,6 +46,8 @@ int main()
 	FILE * fpf = NULL;
 	hnode * h;
 	post * p;
+	int count = 0;
+	char tmp[10000];
 	char sql[200];
 	int wordcount;
 	char term[200];
@@ -94,7 +96,7 @@ int main()
 	// build table posting use file post directly
 	// use hash table
 	InvFile.MakeHashTable(13023973);
-	while (!feof(fpp)) {
+	/*while (!feof(fpp)) {
 		fscanf(fpp, "%s %d %d", &(term[0]), &docid, &pos);
 		//if (docid > 100) {break;}
 		h = InvFile.FindNode(term);
@@ -108,42 +110,35 @@ int main()
 		}else {
 			p->freq++;
 		}
-	}
-	InvFile.SaveInvFile();
+	}*/
+	while (fgets(tmp, 10000, fpp) != NULL) {
+		// Get the stem, the document identifier and the location
+		sscanf(tmp, "%s %d %d", &(term[0]), &docid, &pos);
 
-	/*
-	// or creat index on docid in database
-	sprintf(sql, "select * from post");
-	res = mysql_query(&InvFile.conn,sql);
-	if (!res) {
-		InvFile.result_1 = mysql_store_result(&InvFile.conn);
-		if (!InvFile.result_1) {
-			while (InvFile.sql_row = mysql_fetch_row(InvFile.result_1)) {
-				term = _strdup(InvFile.sql_row[1]);
-				docid = stoi(InvFile.sql_row[2]);
-
-				// find whether term is in table posting
-				sprintf(sql, "select * from posting where term = \"%s\"", term);
-				res = mysql_query(&InvFile.conn, sql);
-				if (!res) {
-					InvFile.result_2 = mysql_store_result(&InvFile.conn);
-					
-					// if term is not in table posting, then insesrt into a new term record
-					if (InvFile.result_2) {
-						sprintf(sql, "insert into posting set term=\"%s\", docid=%d, freq=(select count(*) from post where term=\"%s\" and docid=%d)", term, docid, term, docid);
-						printf("%s", sql);
-						if (mysql_query(&InvFile.conn, sql)) {
-							printf("error:%s\n", mysql_error(&InvFile.conn));
-						}
-					}
-				}
-			}
+		// Add posting into the Integrated Inverted index
+		// See lecture notes on Implementation
+		h = InvFile.FindNode(term);
+		if (!h) {
+			h = InvFile.MakeNode(term);
 		}
+		h->df++;
+		p = InvFile.FindPost(h, docid);
+		if (!p) {
+			p = InvFile.AddPost(h, docid, 1);
+		}
+		else {
+			p->freq++;
+		}
+		// Keep us informed about the progress
+		count++;
+		if ((count % 100000) == 0) printf("Added [%d]\r\n", count);
 	}
-	*/
+
+	printf("Saving inverted file ...\r\n");
+	InvFile.WriteInvFile("..//InvFile.txt");
 
 	// read file "file" into table doclen
-	while (!feof(fpf)) {
+	/*while (!feof(fpf)) {
 		fscanf(fpf, "%d %d @ %s %s", &docid, &wordcount, &(name[0]), path);
 		path = replaceAll(&(path[0]), '\\', "\\\\");
 		//printf("%s", path);
@@ -156,7 +151,7 @@ int main()
 			printf("error:%s::[%d,\"%s\",%d,\"%s\"]\n", mysql_error(&InvFile.conn), docid, &(name[0]), wordcount, path);
 		}
 	}
-
+	*/
     return 0;
 }
 
